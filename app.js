c3d8fd6c075e2deb81d65f300a5f1775dcd47455
padma chockalingam <jmcpadma@gmail.com>
2017-11-28 12:04:45 +0530

    1234

--- app.js
+++ app.js
@@ -55,3 +55,50 @@
 // fs.unlink('writeme.txt',function(){
 //     fs.rmdir('stuff');
 // });
+var http = require('http');
+
+var fs = require('fs');
+
+// var myReadStream = fs.createReadStream(__dirname + '/readme.txt','utf8');
+// var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
+
+// myReadStream.pipe(myWriteStream);//pipe creation
+// myReadStream.on('data', function(chunk){
+// 	console.log('new chunk received:');
+// 	myWriteStream.write(chunk);
+	
+// 	// console.log(chunk);
+// });
+// server creation example
+var server = http.createServer(function(req,res){
+	console.log('request was made: '+ req.url);
+	if(req.url === '/home' || req.url === '/'){
+		res.writeHead(200, {'Content-Type':'text/html'});
+		fs.createReadStream(__dirname +'/index.html').pipe(res);
+		
+	}else if(req.url === '/contact'){
+		res.writeHead(200,{'Content-Type':'text/html'});
+		fs.createReadStream(__dirname +'/contact.html').pipe(res);
+	}else if(req.url === '/api'){
+		var myObj = [{name:'ryu',job:'ninja',age:100},{name:'joshi',job:'ninja',age:200}];
+		res.writeHead(200, {'Content-Type':'application/json'});
+		res.end(JSON.stringify(myObj));
+	}else{
+		res.writeHead(404, {'Content-Type':'text/html'});
+		fs.createReadStream(__dirname +'/404.html').pipe(res);
+	}
+	// res.writeHead(200,{'Content-Type': 'application/json'});
+	// var myObj = {
+	// 	name:'ryu',
+	// 	job:'ninja',
+	// 	age:100
+	// };
+	// res.end(JSON.stringify(myObj));
+	// var myReadStream = fs.createReadStream(__dirname + '/index.html','utf8');
+	// myReadStream.pipe(res);
+	// res.end('hey ninjas');
+});
+
+
+server.listen(3000,'127.0.0.1');
+console.log('listening to port 3000');
