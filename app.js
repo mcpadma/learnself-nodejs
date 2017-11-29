352f0a598a8479d86006ad09fe948f12c4a62175
padma chockalingam <jmcpadma@gmail.com>
2017-11-29 16:46:06 +0530

    234

--- app.js
+++ app.js
@@ -1,104 +1,136 @@
 
-// event emitter code example
-// var events = require('events');
-// // var myEmitter = new events.EventEmitter();
-// // myEmitter.on('someEvent',function(mssg){
-// //     console.log(mssg);
+// // event emitter code example
+// // var events = require('events');
+// // // var myEmitter = new events.EventEmitter();
+// // // myEmitter.on('someEvent',function(mssg){
+// // //     console.log(mssg);
+// // // });
+// // // myEmitter.emit('someEvent','the event was emitted');
+// // var util = require('util');
+// // var person = function(name){
+// //     this.name = name;
+// // };
+// // util.inherits(person,events.EventEmitter);
+// // var james = new person('james');
+// // var johny = new person('john');
+// // var marry = new person('marry');
+// // var people = [james, johny, marry];
+// // people.forEach(function(person){
+// //     person.on('speak',function(mssg){
+// //         console.log(person.name +  ' said ' +mssg)
+// //     })
 // // });
-// // myEmitter.emit('someEvent','the event was emitted');
-// var util = require('util');
-// var person = function(name){
-//     this.name = name;
-// };
-// util.inherits(person,events.EventEmitter);
-// var james = new person('james');
-// var johny = new person('john');
-// var marry = new person('marry');
-// var people = [james, johny, marry];
-// people.forEach(function(person){
-//     person.on('speak',function(mssg){
-//         console.log(person.name +  ' said ' +mssg)
-//     })
-// });
-// james.emit('speak','hey dudes');
-// johny.emit('speak','hi');
-// // element.on('click',function(){})
+// // james.emit('speak','hey dudes');
+// // johny.emit('speak','hi');
+// // // element.on('click',function(){})
 
 
 
-// reading and writing file code example
-// var  fs = require('fs');
-// sync method
-// var readme = fs.readFileSync('readme.txt', 'utf8');
-// // console.log(readme);
-// fs.writeFileSync('writeme.txt', readme);
-// Async method
-// fs.readFile('readme.txt','utf8',function(err,data){
-//     // console.log(data);
-//     fs.writeFile('writeme.txt',data);
-// });
-// deleting file
-// fs.unlink('writeme.txt');
-// console.log('text');
+// // reading and writing file code example
+// // var  fs = require('fs');
+// // sync method
+// // var readme = fs.readFileSync('readme.txt', 'utf8');
+// // // console.log(readme);
+// // fs.writeFileSync('writeme.txt', readme);
+// // Async method
+// // fs.readFile('readme.txt','utf8',function(err,data){
+// //     // console.log(data);
+// //     fs.writeFile('writeme.txt',data);
+// // });
+// // deleting file
+// // fs.unlink('writeme.txt');
+// // console.log('text');
 
 
 
-// creating directory
-// var  fs = require('fs');
-// fs.mkdirSync('stuff');//to create a folder or directory
-// fs.rmdirSync('stuff');//to remove a folder or directory
-// fs.mkdir('stuff',function(){
-//     fs.readFile('readme.txt','utf8',function(err,data){
-//         fs.writeFile('writeme.txt',data);
-//     });
-// });
-// fs.unlink('writeme.txt',function(){
-//     fs.rmdir('stuff');
-// });
-var http = require('http');
+// // creating directory
+// // var  fs = require('fs');
+// // fs.mkdirSync('stuff');//to create a folder or directory
+// // fs.rmdirSync('stuff');//to remove a folder or directory
+// // fs.mkdir('stuff',function(){
+// //     fs.readFile('readme.txt','utf8',function(err,data){
+// //         fs.writeFile('writeme.txt',data);
+// //     });
+// // });
+// // fs.unlink('writeme.txt',function(){
+// //     fs.rmdir('stuff');
+// // });
+// var http = require('http');
 
-var fs = require('fs');
+// var fs = require('fs');
 
-// var myReadStream = fs.createReadStream(__dirname + '/readme.txt','utf8');
-// var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
+// // var myReadStream = fs.createReadStream(__dirname + '/readme.txt','utf8');
+// // var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
 
-// myReadStream.pipe(myWriteStream);//pipe creation
-// myReadStream.on('data', function(chunk){
-// 	console.log('new chunk received:');
-// 	myWriteStream.write(chunk);
+// // myReadStream.pipe(myWriteStream);//pipe creation
+// // myReadStream.on('data', function(chunk){
+// // 	console.log('new chunk received:');
+// // 	myWriteStream.write(chunk);
 	
-// 	// console.log(chunk);
-// });
-// server creation example
-var server = http.createServer(function(req,res){
-	console.log('request was made: '+ req.url);
-	if(req.url === '/home' || req.url === '/'){
-		res.writeHead(200, {'Content-Type':'text/html'});
-		fs.createReadStream(__dirname +'/index.html').pipe(res);
+// // 	// console.log(chunk);
+// // });
+// // server creation example
+// var server = http.createServer(function(req,res){
+// 	console.log('request was made: '+ req.url);
+// 	if(req.url === '/home' || req.url === '/'){
+// 		res.writeHead(200, {'Content-Type':'text/html'});
+// 		fs.createReadStream(__dirname +'/index.html').pipe(res);
 		
-	}else if(req.url === '/contact'){
-		res.writeHead(200,{'Content-Type':'text/html'});
-		fs.createReadStream(__dirname +'/contact.html').pipe(res);
-	}else if(req.url === '/api'){
-		var myObj = [{name:'ryu',job:'ninja',age:100},{name:'joshi',job:'ninja',age:200}];
-		res.writeHead(200, {'Content-Type':'application/json'});
-		res.end(JSON.stringify(myObj));
-	}else{
-		res.writeHead(404, {'Content-Type':'text/html'});
-		fs.createReadStream(__dirname +'/404.html').pipe(res);
-	}
-	// res.writeHead(200,{'Content-Type': 'application/json'});
-	// var myObj = {
-	// 	name:'ryu',
-	// 	job:'ninja',
-	// 	age:100
-	// };
-	// res.end(JSON.stringify(myObj));
-	// var myReadStream = fs.createReadStream(__dirname + '/index.html','utf8');
-	// myReadStream.pipe(res);
-	// res.end('hey ninjas');
+// 	}else if(req.url === '/contact'){
+// 		res.writeHead(200,{'Content-Type':'text/html'});
+// 		fs.createReadStream(__dirname +'/contact.html').pipe(res);
+// 	}else if(req.url === '/api'){
+// 		var myObj = [{name:'ryu',job:'ninja',age:100},{name:'joshi',job:'ninja',age:200}];
+// 		res.writeHead(200, {'Content-Type':'application/json'});
+// 		res.end(JSON.stringify(myObj));
+// 	}else{
+// 		res.writeHead(404, {'Content-Type':'text/html'});
+// 		fs.createReadStream(__dirname +'/404.html').pipe(res);
+// 	}
+// 	// res.writeHead(200,{'Content-Type': 'application/json'});
+// 	// var myObj = {
+// 	// 	name:'ryu',
+// 	// 	job:'ninja',
+// 	// 	age:100
+// 	// };
+// 	// res.end(JSON.stringify(myObj));
+// 	// var myReadStream = fs.createReadStream(__dirname + '/index.html','utf8');
+// 	// myReadStream.pipe(res);
+// 	// res.end('hey ninjas');
+// });
+
+
+// server.listen(3000,'127.0.0.1');
+// console.log('listening to port 3000');
+
+
+// using express
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.set('view engine','ejs');
+app.use('/assets',express.static('assets'));
+
+app.get('/',function(req,res){
+	res.render('index');
+});
+app.get('/contact',function(req,res){
+	console.log(req.query);
+	res.render('contact',{qs: req.query});
+});
+app.post('/contact',urlencodedParser,function(req,res){
+	console.log(req.body);
+	res.render('contact-success',{data: req.body});
 });
 
+app.get('/profile/:name',function(req,res){
+	var data = {age: 29, job:'ninja', hobbies:['eating','drawing','painting']};
+	res.render('profile',{person: req.params.name, data: data});
+	// res.send('you requested to a profile with id '+  req.params.id);
+});
 
-server.listen(3000,'127.0.0.1');
-console.log('listening to port 3000');
+app.listen(3000);
